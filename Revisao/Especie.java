2833df723ccdd7b052745f2a6dc016f6434ec800
IsabelaxBorges <126192407+IsabelaxBorges@users.noreply.github.com>
2023-05-09 11:28:28 -0300

    Add files via upload

---
+++ Revisao/Especie.java
@@ -0,0 +1,18 @@
+public class Especie {
+
+    private String nome;
+    private int tamanho_maximo;
+    private String velocidade_maxima;
+    private String dieta;
+
+
+    public Especie(String nome, int tamanho_maximo, String velocidade_maxima, String dieta){
+        //Inicando os atributos
+        this.nome = nome;
+        this.tamanho_maximo = tamanho_maximo;
+        this.velocidade_maxima = velocidade_maxima;
+        this.dieta = dieta;        
+
+    }
+    
+}
---
+++ Revisao/revisao.java
@@ -0,0 +1,28 @@
+/*Diagrama de classes: descrever como nosso código funciona
+ * public, protedec e default  + # ~ = padrões de visibilidades - Atençaõ se é private ou não 
+ * 
+ * 
+ * 
+ * 
+ * operações são nossos metódos com tipos de  retorno ^
+ * uma classe é composta por Atributos e métodos
+ * Atributos String, int, double ..
+ * Métodos: public void envelhecer (){}
+ * Dentro dos () vai os parametros  
+ * Exemplo public void recolorir (String novaCor){  }
+ * 
+ * ///Construtor tem o mesnomo nome que a classe, não tem retorno. Sempre vai ser public e o nome da classe. Construtor é um método 
+ * 
+ * Atributos compsotos: são um relacionamento entre duas classes. uma associação. Multiplicidades uma animal pertence a especie mas uma especie tem muitos animais.
+ * a seta é o animal vai acessar as coisas das especies. o animal vai chamar os métodos/atributos  das especies. Só  uma linha é que os dois tem atributos um do outro.
+ * 
+ * 
+ *  Agregação, em java implemnetamos atrás do arraylist  
+ * 
+ * lozangolo = agragação
+ * 
+ * 
+ */
+
+
+ 
